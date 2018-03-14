1f28db0abc3441900cf4e52d40e34690d3043efe
Franz08 <blicanel@yahoo.com>
2018-03-14 13:38:02 +0800

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyAn9SQBuvTtoQhNl-Ot5EDXQChkqoQeIDw",
+        authDomain: "firestarter-char.firebaseapp.com",
+        databaseURL: "https://firestarter-char.firebaseio.com",
+        projectId: "firestarter-char",
+        storageBucket: "firestarter-char.appspot.com",
+        messagingSenderId: "206115761017"
     }
 };
